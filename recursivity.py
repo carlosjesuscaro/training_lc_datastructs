d291e17e3ed38c59955a49fa31b24cbd9120ca1d
Carlos Jesus Caro <carlos.jesus.caro@gmail.com>
2024-12-16 20:47:21 -0500

    feat: basic docker setup with recursive excercise

---
+++ recursivity.py
@@ -0,0 +1,29 @@
+from timeit import timeit
+
+
+def normal_loop(num: int) -> None:
+    for i in range(num):
+        print(i)
+
+def with_recursion(num: int) -> None:
+    if num == 0:
+        print(num)
+        return
+    with_recursion(num - 1)
+    print(num)
+
+def recursive_reverse(num: int) -> None:
+    print(num)
+    if (num - 1) == -1:
+        return
+    recursive_reverse(num - 1)
+
+def recursive_main() -> None:
+    number = 50
+    result = timeit(f'normal_loop({50})', globals=globals(), number=100)
+    recursive_time = timeit(f'with_recursion({number})', globals=globals(), number=100)
+    reverse_time = timeit(f'recursive_reverse({number})', globals=globals(), number=100)
+    print(f"Loop execution time: {result:.6f} seconds")
+    print(f"Recursion execution time: {recursive_time:.6f} seconds")
+    print(f"Reverse execution time: {reverse_time:.6f} seconds")
+    return None
