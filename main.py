db7a6ea7d3d85f7031dde0b55183220d1572e40f
Carlos Jesus Caro <carlos.jesus.caro@gmail.com>
2025-05-25 15:58:19 -0400

    feat: Two array sum completed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from recursivity import recursive_main
 from palindrome import palindrome_main
 from two_sum import two_sum_main
+from two_array_sorted import combine
 
 if __name__ == '__main__':
     print('Recursive exercise:')
@@ -9,4 +10,6 @@ if __name__ == '__main__':
     palindrome_main()
     print('\nTwo sum exercise')
     two_sum_main()
+    print('\nTwo array sum exercise')
+    print(combine([1, 2, 3, 150, 450], [100, 200, 300, 302, 500]))
 
--- two_array_sorted.py
+++ two_array_sorted.py
@@ -1,14 +1,21 @@
-def compare(a:int, b:int, x:list) -> list:
-    if a<b:
-        x.append(a)
-    else:
-        x.append(b)
-    
+def combine(arr1, arr2):
+    # ans is the answer
+    ans = []
+    i = j = 0
+    while i < len(arr1) and j < len(arr2):
+        if arr1[i] < arr2[j]:
+            ans.append(arr1[i])
+            i += 1
+        else:
+            ans.append(arr2[j])
+            j += 1
 
-def two_array_main(arr1:list, arr2:list) -> list:
-    new_arr=[]
-    for i in range(max(len(arr1), len(arr2))):
-        compare(arr1[i], arr2[i], new_arr)
-    return new_arr
+    while i < len(arr1):
+        ans.append(arr1[i])
+        i += 1
 
-print(two_array_main([1,2,3],[100,200,300]))
+    while j < len(arr2):
+        ans.append(arr2[j])
+        j += 1
+
+    return ans
