ce07b04fd1d8096345188587f42df9cbcae6df58
Carlos Jesus Caro <carlos.jesus.caro@gmail.com>
2025-05-27 23:01:48 -0400

    feat: squared sorted v2

--- arrays/sorted_square.py
+++ arrays/sorted_square.py
@@ -1,46 +1,16 @@
-def sorted_squares(nums: list[int]) -> list[int]:
-    for i in range(len(nums)):
-        nums[i] = nums[i] ** 2
-
-    for i in range(len(nums)):
-        for j in range(i,len(nums)):
-            if nums[i] > nums[j]:
-                nums[i], nums[j] = nums[j], nums[i]
-
-    return nums
-
-# aa = sorted_squares([-100, -4, -1, 0, 3, 10, 101])
-aa = sorted_squares([-4,-4,-3])
-print(aa)
-
-def new_sorted_sq(nums: list[int]) -> list[int]:
-    j = -1
-    check = True
-    while check:
-        flag = True
-        if nums[0] < 0:
-            nums[0] = -nums[0]
-            while flag:
-                if nums[0] <= nums[j]:
-                    if j != -len(nums):
-                        j -= 1
-                    else:
-                        break
-                else:
-                    nums.insert(j, nums[0])
-                    nums[j], nums[j - 1] = nums[j - 1], nums[j]
-                    nums.pop(0)
-                    break
+def efficient(nums: list[int])->list[int]:
+    new_nums = [0] * len(nums)
+    i, j, z = 0, len(nums) - 1, len(nums) - 1
+    while i <= j:
+        if abs(nums[i]) >= abs(nums[j]):
+            new_nums[z] = nums[i]**2
+            i += 1
         else:
-            break
+            new_nums[z] = nums[j] ** 2
+            j -= 1
+        z -= 1
+    return new_nums
 
-    for i in range(len(nums)):
-        nums[i] = nums[i] ** 2
-    return nums
-
-
-# bb = new_sorted_sq([-10, -9, -8, -5, -3, 0, 1, 2, 3, 4, 5, 9])
-# bb = new_sorted_sq([-4, -4, -3])
-bb = new_sorted_sq([-4,-1,0,3,10])
-print(bb)
+cc = efficient([-4,-1,0,3,10])
+print(cc)
 
