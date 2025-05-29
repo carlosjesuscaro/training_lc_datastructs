a98f9af6e0916d55ec3d637c963f072d77bc97f9
Carlos Jesus Caro <carlos.jesus.caro@gmail.com>
2025-05-28 23:06:01 -0400

    fix: Updating the subsequent solution

--- arrays/subsequence.py
+++ arrays/subsequence.py
@@ -1,15 +1,19 @@
 def subsequence(small: str, big: str) -> bool:
-    status, i = False, 0
-    while not status:
-        for j in range(len(big)):
-            if small[i] == big[j]:
-                i+=1
-                if i == len(small):
-                    status = True
-                    break
-        break
+    status, i, j = False, 0, 0
+    while j < len(big):
+        if small[i] == big[j]:
+            i += 1
+            j += 1
+        else:
+            j += 1
+
+        if i == len(small):
+            status = True
+            break
     return status
 
 def subsequence_main():
     print(f"Subsequent status: {subsequence('nalu', 'alianzanaluwaterloo')}")
+
+
 
--- tests/test_subsequence.py
+++ tests/test_subsequence.py
@@ -2,3 +2,4 @@ from arrays.subsequence import subsequence
 
 def test_subsequence():
     assert subsequence('alianza', 'alianzalima') == True
+    assert subsequence('banana', 'anana') == False
